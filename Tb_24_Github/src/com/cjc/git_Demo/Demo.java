4d6815361d5d8487d5c782cff7030bcab581a208
HP <HP@DESKTOP-1S8UOP2>
2024-04-01 12:38:52 +0530

    By suraj

---
+++ Tb_24_Github/src/com/cjc/git_Demo/Demo.java
@@ -0,0 +1,20 @@
+package com.cjc.git_Demo;
+
+public class Demo {
+	public void m1() 
+	{
+		System.out.println("M1 method");
+	}
+	
+	public void m2() 
+	{
+		System.out.println("M2 method");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Git Demo");
+	}
+
+}
