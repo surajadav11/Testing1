7a51d3e76737d9dff80cd4f3c4a4d3f8c2cbdfb1
HP <HP@DESKTOP-1S8UOP2>
2024-04-02 09:26:43 +0530

    By surajAdav

--- Tb_24_Github/src/com/cjc/git_Demo/Demo.java
+++ Tb_24_Github/src/com/cjc/git_Demo/Demo.java
@@ -1,6 +1,10 @@
 package com.cjc.git_Demo;
 
 public class Demo {
+	
+	
+	
+	
 	public void m1() 
 	{
 		System.out.println("M1 method");
@@ -27,6 +31,10 @@ public class Demo {
 		System.out.println("M5 method");
 	}
 
+	public void run()
+	{
+		System.out.println("This is run method");
+	}
 	
 
 	public static void main(String[] args) {
